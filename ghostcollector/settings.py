ac2c90f6855bffa15314c79daa0b1ca45e9dc879
Zahir <zahir.spencer@yahoo.com>
2021-09-07 17:12:03 -0400

    d:

--- ghostcollector/settings.py
+++ ghostcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%0f8@4sz*#&la-$s4c=!u&lk4h90h&z)gjo$7uw5zx1)fog5(@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
